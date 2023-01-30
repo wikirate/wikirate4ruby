265772beba70f12f94b1563bd8fe259aa665d521
Vasiliki Gkatziaki <vasgat@gmail.com>
2023-01-30 15:03:19 +0100

    remove RequestUtils under REST module and make the methods private

--- lib/wikirate4ruby/request_utils.rb
+++ lib/wikirate4ruby/request_utils.rb
@@ -1,36 +1,36 @@
 module Wikirate4ruby
-  module REST
-    module RequestUtils
+  module RequestUtils
 
-      def creation_params(entity, optional_params = {}, allowed_params = [])
-        params = {
-          'card[type]' => entity,
-          'format' => 'json',
-          'success[format]' => 'json'
-        }
+    private
 
-        optional_params.each do |key, value|
-          next if value.nil?
+    def creation_params(entity, optional_params = {}, allowed_params = [])
+      params = {
+        'card[type]' => entity,
+        'format' => 'json',
+        'success[format]' => 'json'
+      }
 
-          if allowed_params.include? key
-            params["card[subcards][+#{key}]"] = (%w[company subject_company object_company].include? key) ? str_identifier(value) : value.to_s
-          else
-            @logger.warn("Unexpected parameter: #{key}")
-          end
+      optional_params.each do |key, value|
+        next if value.nil?
+
+        if allowed_params.include? key
+          params["card[subcards][+#{key}]"] = (%w[company subject_company object_company].include? key) ? str_identifier(value) : value.to_s
+        else
+          @logger.warn("Unexpected parameter: #{key}")
         end
-        params
       end
+      params
+    end
 
-      def error(code, body)
-        Wikirate4ruby::Error::HTTP_ERRORS[code].new(JSON.pretty_generate(body))
-      end
+    def error(code, body)
+      Wikirate4ruby::Error::HTTP_ERRORS[code].new(JSON.pretty_generate(body))
+    end
 
-      def headers(api_key)
-        {
-          'Content-Type' => 'application/json',
-          'X-API-KEY' => api_key
-        }
-      end
+    def headers(api_key)
+      {
+        'Content-Type' => 'application/json',
+        'X-API-KEY' => api_key
+      }
     end
   end
 end

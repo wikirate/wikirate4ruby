0b5769fbb078a2b82b9b64bb6e7536a18aa56210
Vasiliki Gkatziaki <vasgat@gmail.com>
2023-01-30 16:16:54 +0100

    edits on wikirate4ruby.rb

--- lib/wikirate4ruby.rb
+++ lib/wikirate4ruby.rb
@@ -1,7 +1,28 @@
-# frozen_string_literal: true
-
 require_relative "wikirate4ruby/version"
+require_relative "wikirate4ruby/entities/card"
+require_relative "wikirate4ruby/entities/answer"
+require_relative "wikirate4ruby/entities/checked_by"
+require_relative "wikirate4ruby/entities/company_group"
+require_relative "wikirate4ruby/entities/company"
+require_relative "wikirate4ruby/entities/dataset"
+require_relative "wikirate4ruby/entities/metric"
+require_relative "wikirate4ruby/entities/region"
+require_relative "wikirate4ruby/entities/relationship_answer"
+require_relative "wikirate4ruby/entities/source"
+require_relative "wikirate4ruby/entities/topic"
+require_relative "wikirate4ruby/client"
+require_relative "wikirate4ruby/error"
+require_relative "wikirate4ruby/request_utils"
+require_relative "wikirate4ruby/utils"
+require_relative "wikirate4ruby/request"
 
 module Wikirate4ruby
-  # Your code goes here...
+  module REST
+  end
+  module Entities
+  end
+  module RequestUtils
+  end
+  module StringUtils
+  end
 end

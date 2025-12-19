666b5366a0e7f654c721ca4c86e3b00eff9fc2b3
vasgat <vasgat@gmail.com>
2025-12-19 15:01:12 +0100

    fix(WDM-15): small updates on tests

--- test/test_wikirate4ruby.rb
+++ test/test_wikirate4ruby.rb
@@ -34,7 +34,7 @@ class TestWikirate4ruby < Minitest::Test
   end
 
   def test_get_topic
-    assert @client.get_topic("Environment").is_a? Topic
+    assert @client.get_topic("Wikirate ESG Topics+Environment").is_a? Topic
   end
 
   def test_get_source
@@ -50,7 +50,7 @@ class TestWikirate4ruby < Minitest::Test
   end
 
   def test_get_project
-    assert @client.get_project(14_495_617).is_a? Card
+    assert @client.get_project(22801903).is_a? Card
   end
 
   def test_get_dataset
@@ -70,13 +70,14 @@ class TestWikirate4ruby < Minitest::Test
   end
 
   def test_get_answers
-    answers = @client.get_answers("Direct_greenhouse_gas_GHG_emissions_Scope_1_GRI_305_1_a_formerly_G4_EN15_a", "Global_Reporting_Initiative")
+    answers = @client.get_answers(metric_name: "Direct_greenhouse_gas_GHG_emissions_Scope_1_GRI_305_1_a_formerly_G4_EN15_a",
+                                  metric_designer: "Global_Reporting_Initiative")
     assert answers.length == 20
     assert answers[0].is_a? Answer
   end
 
   def test_get_relationships
-    answers = @client.get_relationships("Supplied_by", "Commons")
+    answers = @client.get_relationships(metric_name: "Supplied_by", metric_designer: "Commons")
     assert answers.length == 20
     assert answers[0].is_a? Relationship
   end
